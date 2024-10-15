8756e82fd9ee7c5db0b1feaa56c186a2de48a094
JuliaRj <julia329693@gmail.com>
2024-10-15 11:11:59 +0300

    task 7

---
+++ 7/main.js
@@ -0,0 +1,39 @@
+
+//  ДЗ 7.1. Замикання
+function getSum(){
+    let sum = 0;
+    return function (value) {
+        sum += value;
+        return sum;
+    };
+}
+const sum = getSum();
+console.log(sum(2));
+console.log(sum(3));
+
+//  ДЗ 7.2. Добуток через карування
+function multiply (firstNumber) {
+return function (secondNumber) {
+    return firstNumber * secondNumber;
+    }
+}
+console.log (multiply(5)(2));
+
+//  ДЗ 7.3. Функція з циклом
+function getNumber() {
+    let number;
+    for(let i = 0; i < 10; i++){
+        userNumber = prompt("Введіть число більше 100");
+        if (userNumber === null) {
+            console.log("Користувач вийшов");
+            return null;}  else 
+             if (!isNaN(userNumber) && userNumber > 100 ){
+                number = userNumber;
+          
+        }
+    }
+    console.log("Максимальна кількість спроб! Останній номер:", userNumber);
+    return  number;
+    
+}
+getNumber();
