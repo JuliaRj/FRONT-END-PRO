1e43a782e6752545d102741e6be97c8025c47015
JuliaRj <julia329693@gmail.com>
2024-10-24 23:09:51 +0300

    task 7

---
+++ 10/main.js
@@ -0,0 +1,26 @@
+
+//  ДЗ 10.1. Картка користувача
+const user ={
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    city: 'Odessa',
+   getInfo: function () {
+    
+    for (const key in user) {
+       
+        if (typeof user[key] !== 'function'){
+            console.log(`${key}: ${user[key]}`);
+        }
+      }
+       return this;
+   }
+}
+user.getInfo();
+
+
+//  ДЗ 10.2. Отримання парних чисел з масиву
+
+
+
+//  ДЗ 10.3. Книга контактів
