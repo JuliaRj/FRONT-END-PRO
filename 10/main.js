da78b91a1ee2ea8b0af75471cad6c6514339f659
JuliaRj <julia329693@gmail.com>
2024-10-25 09:05:10 +0300

    task-10

--- 10/main.js
+++ 10/main.js
@@ -30,3 +30,32 @@ for (const number of mass) {
   };
 console.log(newMass);
 //  ДЗ 10.3. Книга контактів
+const phoneBook = {
+    contacts: [
+        {
+            name: "Julia Shvets",
+            phone: "+35145015215",
+            email: "myauk@.gmail.com"
+        },
+        {
+            name: "John Wick",
+            phone: "+3515322215",
+            email: "aufff@.gmail.com"
+        },
+        {
+            name: "Taras Shevchenko",
+            phone: "+3515245215",
+            email: "My_thirteenth_year_was_wearing_on@.gmail.com"
+        },
+    ],
+    findContact(findName){
+   return this.contacts.find(contact => contact.name === findName)
+},
+    addContact(name, phone, email){
+     this.contacts.push({name, phone, email})
+ }
+};
+
+console.log(phoneBook.findContact("John Wick"));
+phoneBook.addContact("Freddy Krueger", "+3451515441", "freddy_will_come_for_you@mail.com");
+console.log(phoneBook.findContact("Freddy Krueger"));
