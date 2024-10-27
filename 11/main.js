407cea4604852475d7a7f50cf7b20510f77cdeb8
JuliaRj <julia329693@gmail.com>
2024-10-27 15:18:24 +0200

    task-11

--- 11/main.js
+++ 11/main.js
@@ -1,25 +1,39 @@
-
-//  ДЗ 11.1. Таблиця піфагора
-
-
-
-
-//  ДЗ 11.2. Кнопка зміни кольору
-const txt = document.querySelector(".main_txt");
-const button = document.querySelector(".btn");
-button.onclick = function(){
-  txt.classList.toggle("color_txt");
-};
-
-
-//  ДЗ 11.3. Отримання випадкового зображення
-            const images = document.querySelector('.images');
-            const randomNum = Math.floor(Math.random() * 9) + 1;
-            const newImg = document.createElement('img');
-        function showRandomImage() {
-            newImg.src = 'img/' + randomNum + '.jpg';
-            newImg.alt = 'New img';
-
-            images.appendChild(newImg);
-        }
-        showRandomImage();
+ //  ДЗ 11.1. Таблиця піфагора
+ const tabl = document.querySelector('.tabl');
+ const newTabl = document.createElement('table');
+ 
+ function showPifTable() {
+ 
+     for (let i = 1; i <= 10; i++) {
+         const tr = document.createElement('tr');
+         newTabl.appendChild(tr);
+         for (let j = 1; j <= 10; j++) {
+             const td = document.createElement('td');
+             tr.appendChild(td);
+             td.textContent = i * j;
+         }
+     }
+ 
+     tabl.appendChild(newTabl);
+ }
+ showPifTable();
+ 
+ //  ДЗ 11.2. Кнопка зміни кольору
+ const txt = document.querySelector(".main_txt");
+ const button = document.querySelector(".btn");
+ button.onclick = function(){
+   txt.classList.toggle("color_txt");
+ };
+ 
+ 
+ //  ДЗ 11.3. Отримання випадкового зображення
+             const images = document.querySelector('.images');
+             const randomNum = Math.floor(Math.random() * 9) + 1;
+             const newImg = document.createElement('img');
+         function showRandomImage() {
+             newImg.src = 'img/' + randomNum + '.jpg';
+             newImg.alt = 'New img';
+ 
+             images.appendChild(newImg);
+         }
+         showRandomImage();
