ec640e8999fc73ea406d3055b546747c6d77727d
JuliaRj <julia329693@gmail.com>
2024-10-27 14:00:59 +0200

    task-11

--- 11/main.js
+++ 11/main.js
@@ -3,13 +3,23 @@
 
 
 
+
 //  ДЗ 11.2. Кнопка зміни кольору
-const color = document.querySelector(".color_txt");
-const txt = document.querySelector(".mein_txt");
-const button = document.querySelector(".change_color_btn");
-button.onclick = function () {
-  txt.classList.toggle("color")
+const txt = document.querySelector(".main_txt");
+const button = document.querySelector(".btn");
+button.onclick = function(){
+  txt.classList.toggle("color_txt");
 };
 
 
 //  ДЗ 11.3. Отримання випадкового зображення
+            const images = document.querySelector('.images');
+            const randomNum = Math.floor(Math.random() * 9) + 1;
+            const newImg = document.createElement('img');
+        function showRandomImage() {
+            newImg.src = randomNum + '.jpg';
+            newImg.alt = 'New img';
+
+            images.appendChild(newImg);
+        }
+        showRandomImage();
